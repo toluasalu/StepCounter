f9a82a404f9a91e197d20f01374b70d58888e16f
toluasalu <johnasalu13@gmail.com>
2023-09-16 17:38:30 +0100

    Render Health Stats

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+type StatsProps = {
+  label: string;
+  value: string | number;
+};
+
+const Stats = ({ label, value }: StatsProps) => {
+  return (
+    <View style={styles.valueContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ flexDirection: "row", gap: 15 }}>
+        <Stats label={"Steps"} value={"1000"} />
+        <Stats label={"Distance"} value={"0.75 km"} />
+      </View>
+
+      <Stats label={"Flights Climbed"} value={"20"} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    padding: 12,
+    justifyContent: "center",
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+  },
+  value: {
+    color: "#AFB3BE",
+    fontSize: 35,
+    fontWeight: "500",
+  },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
   },
 });
